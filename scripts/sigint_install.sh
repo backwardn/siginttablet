7d3b9260d35821e2e44a0714a48f91213c3cbb06
Josh Conway <jwcrawley@gmail.com>
2019-03-27 11:41:06 +0000

    added pyqt5-dev , changed j2 to j4 since rpi 3b+ is quad.

--- scripts/sigint_install.sh
+++ scripts/sigint_install.sh
@@ -15,7 +15,7 @@ echo "Installing SigInt repository software and libraries"
 sudo apt-get install -qq -y build-essential cmake libpcap-dev libpcap0.8 libusb-1.0-0 libnetfilter-queue-dev libnetfilter-queue1 default-jdk apt-file libudh-dev libboost-all-dev libsndfile1-dev imagemagick libfftw3-dev buffer vim libatlas-base-dev wireshark wireshark-qt swig
 sudo apt-get install -qq -y gqrx-sdr gnuradio* librtlsdr-dev soapysdr-module-rtlsdr gr-air-modes gr-radar gr-rds gr-iio gr-hpsdr gr-osmosdr
 sudo apt-get install -qq -y python3-numpy python3-psutil python3-zmq python3-pyqt5 g++ libpython3-dev python3-pip cython3 qt5-default
-sudo apt-get install -qq -y libfftw3-dev pkg-config libliquid-dev sdcc binutils python python-pip doxygen python-numpy python-scipy python-scapy
+sudo apt-get install -qq -y libfftw3-dev pkg-config libliquid-dev sdcc binutils python python-pip doxygen python-numpy python-scipy python-scapy pyqt5-dev
 sudo apt-get install -qq -y libqwtplot3d-qt4-0v5 libqwtplot3d-qt4-dev libqwt-dev libqwt-headers
 sudo pip3 install -U tensorflow
 sudo pip install -U pip
@@ -116,7 +116,7 @@ else
 fi
 cd build
 cmake ..
-make
+make -j4
 sudo make install
 cd ~/source
 
@@ -163,7 +163,7 @@ fi
 mkdir build
 cd build
 cmake ..
-make -j2
+make -j4
 sudo make install
 sudo ldconfig
 cd ~/source
@@ -192,7 +192,7 @@ fi
 mkdir build
 cd build
 cmake ..
-make -j2
+make -j4
 sudo make install
 cd ~/source
 
@@ -209,7 +209,7 @@ fi
 mkdir build
 cd build
 cmake ..
-make -j2
+make -j4
 sudo make install
 cd ~/source
 
