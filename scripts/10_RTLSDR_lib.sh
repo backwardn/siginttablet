e6983886392ed82294cfb38b2acb30914cbbe82e
Josh Conway <jwcrawley@gmail.com>
2019-04-27 23:17:09 -0400

    fixed rtlsdr

--- scripts/10_RTLSDR_lib.sh
+++ scripts/10_RTLSDR_lib.sh
@@ -14,4 +14,6 @@ cd build
 cmake ../ -DINSTALL_UDEV_RULES=ON
 make -j3
 sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo sh -c "echo dvb_usb_rtl28xxu > /etc/modprobe.d/Blacklist-rtl.conf"
 cd ~/source
